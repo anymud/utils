db10bb94f853f73ed17f9770e15c97bac99cdb53
shtse8 <shtse8@outlook.com>
2024-02-26 19:46:53 +0800

    Update build.ts and package.json

--- build.ts
+++ build.ts
@@ -2,50 +2,7 @@ import consola from "consola"
 import fs from "fs/promises"
 import path from "path"
 import Bun  from 'bun'
-import {
-	type CompilationOptions,
-	type EntryPointConfig,
-	generateDtsBundle,
-} from 'dts-bundle-generator';
-import { getTsconfig } from 'get-tsconfig';
-
-function zip<A, B>(a: A[], b: B[]): [A, B][] {
-    return a.map((v, i) => [v, b[i]]);
-}
-
-function dtsPlugin(options?: CompilationOptions) {
-    return {
-        name: "dts",
-        async setup(build: Bun.PluginBuilder) {
-            consola.info(build)
-                
-            const tsconfig = getTsconfig()
-            if (!tsconfig) {
-                consola.warn('No tsconfig found')
-                return
-            }
-            
-            const entries: EntryPointConfig[] = build.config.entrypoints.map((entry) => ({
-                filePath: entry,
-            }))
-            const config = {
-                preferredConfigPath: tsconfig.path,
-                ...options,
-            } as CompilationOptions
-            const srcDir = build.config.root ?? 'src'
-            const outDir = build.config.outdir ?? 'dist'
-            const result = generateDtsBundle(entries, config)
-            const zippedResult = zip(entries.map((x) => x.filePath), result)
-            for (const [filePath, content] of zippedResult) {
-                const relativePath = path.relative(srcDir, filePath);
-                const destPath = path.join(outDir, relativePath).replace(/\.ts$/, '.d.ts');
-                consola.info('Writing', destPath, content.length, 'bytes');
-                consola.info('Writing', destPath, content.length, 'bytes')
-                await Bun.write(destPath, content)
-            }
-        }
-    }
-}
+import { dts } from '@anymud/bun-plugin-dts'
 
 try {
     const distPath ='dist'
@@ -69,11 +26,10 @@ try {
         ],
         outdir: distPath,
         external: [
-            // 'playwright',
             '*',
         ],
         plugins: [
-            dtsPlugin(),
+            dts(),
         ],
         root: './src',
     })
